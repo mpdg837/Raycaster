6cbd4c0c901eb6b66220e0ec873960dbd9451feb
@mpodgajn <michal.podgajny.stud@pw.edu.pl>
2021-07-27 06:57:42 +0200

    Poprawki 2

--- src/Raycaster/Display/UI/ScreenSprite.java
+++ src/Raycaster/Display/UI/ScreenSprite.java
@@ -24,7 +24,8 @@ public class ScreenSprite {
     }
 
     public void draw(Graphics g,long timeStart){
-        g.drawImage(gun,ren.renderSize.x/2-98,ren.renderSize.y-196,196,196,null);
+        Image scaled = gun.getScaledInstance(196,196,Image.SCALE_FAST);
+        g.drawImage(scaled,ren.renderSize.x/2-98,ren.renderSize.y-196,null);
 
         final long timeEnd = System.nanoTime()/1000000;
         final int deltaRenderTime = (int)(timeEnd - timeStart);
